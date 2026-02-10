8b865d6d51971df6d25f6dd1d0d9ff1504e912f0
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-02-10 22:30:24 +0600

    Deleted unnecessary branches

--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a107375052ed1e802ca4b8d03226cb2b20f68768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdb09c985558585d7440d49a9f161668fb17d7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
